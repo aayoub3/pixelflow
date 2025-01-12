b87bb8883869e9d418b7bd87809b3c5d2a855eee
Abanob Ayoub <abanobkamal369@gmail.com>
2025-01-12 00:15:48 -0800

    add comments to config file

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  //  base: 'pixelflow',  //use this base when you want to deploy on Github
-   base: '/',   //use this base when you want to deploy on Amplify AWS
+  //  base: 'pixelflow',  //use this base when you want to deploy on Github without using custom domain
+   base: '/',   //use this base when you deploy on Amplify AWS OR depoly on github with using custom domain
   plugins: [react()],
   build:{
     outDir: 'dist',
