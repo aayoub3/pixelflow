ff7ad9b1ad4bd20f658c2b7914e087da5ffa1ed8
Abanob Ayoub <abanobkamal369@gmail.com>
2025-01-11 23:54:55 -0800

    deploy to github

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: 'pixelflow',  //use this base when you want to deploy on Github
-   base: '/',   //use this base when you want to deploy on Amplify AWS
+   base: 'pixelflow',  //use this base when you want to deploy on Github
+  //  base: '/',   //use this base when you want to deploy on Amplify AWS
   plugins: [react()],
   build:{
     outDir: 'dist',
