99f1f5ff29ffb9744bbcfe61364513bb8cc61402
Abanob Ayoub <abanobkamal369@gmail.com>
2025-02-20 11:57:37 -0800

    update temp-home page

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,8 +4,8 @@ function Footer(){
     return(
         <footer id='home-footer'>
             <p>Photography & Videography Services <br />
-            FB| facebook.com/pixelflowcreation <br />
-             IG | instagram.com/pixelflowcreation</p>
+            www.facebook.com/pixelflowcreation <br />
+             www.instagram.com/pixelflowcreation</p>
             <p>&copy; {new Date().getFullYear()} PIXEL FLOW</p>
         </footer>
 
