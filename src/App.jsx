86ec9f6a6aa02e175e77882806f866006622d478
Abanob Ayoub <abanobkamal369@gmail.com>
2025-02-20 14:29:45 -0800

    add DisableInspect.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import DisableInspect from "./DisableInspect";
 import Header from "./Header";
 import Footer from "./Footer";
 
 function App() {
   return (
   <>
+  <DisableInspect></DisableInspect>
   <Header></Header>
   <Footer></Footer>
 
