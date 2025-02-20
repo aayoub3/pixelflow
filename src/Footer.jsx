abf61e868e5cda4fa4fe67c9d82db727482b7ba2
Abanob Ayoub <abanobkamal369@gmail.com>
2025-02-19 16:15:39 -0800

    adjusting the home page

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,6 +3,9 @@ import './Footer.css';
 function Footer(){
     return(
         <footer id='home-footer'>
+            <p>Photography & Videography Services <br />
+            FB| facebook.com/pixelflowcreation <br />
+             IG | instagram.com/pixelflowcreation</p>
             <p>&copy; {new Date().getFullYear()} PIXEL FLOW</p>
         </footer>
 
--- src/Header.jsx
+++ src/Header.jsx
@@ -23,12 +23,12 @@ function Header() {
       <img id='home-logo' src={logo} alt="Pixel Flow logo" />
       <h1 id='welcome-message'>COMMING SOON</h1>
       <h1 id="welcome-message2">Stay Tuned<span>{dots}</span></h1>
+      <p id='main-message'>Until further notice, for bookings or inquiries contact us via our social media inbox on Facebook/Instagram or by email.</p>
       <hr />
         <ul>
-          <li> <a href=""> Home </a> </li>
-          <li> <a href=""> About </a> </li>
-          <li> <a href=""> Services </a> </li>
-          <li> <a href=""> Contact </a> </li>
+          <li> <a href="https://www.facebook.com/pixelflowcreation" target="_blank"> Facebook </a> </li>
+          <li> <a href="https://www.instagram.com/pixelflowcreation" target="_blank"> Instagram </a> </li>
+          <li><a href="mailto:pixelflowcreation@gmail.com">Email Us</a></li>
         </ul>
       </nav>
 
